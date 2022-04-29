edb39fb8652360d90f1c6f39b3743eea3eabecb6
vivekkoya <13vivekkoya@gmail.com>
2022-04-29 13:17:07 -0700

    Merged with main

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -50,6 +50,50 @@ public class FamilyTree {
 				curr = curr.parent;
 			}
 
+		// Searches subtree at this node for a node
+		// with the given name. Returns the node, or null if not found.
+		TreeNode getNodeWithName(String targetName) {
+			// Does this node have the target name?
+			if (children.contains(targetName))
+				return this;
+
+			// No, recurse. Check all children of this node.
+			for (TreeNode child : children) {
+				// If child.getNodeWithName(targetName) returns a non-null node,
+				// then that's the node we're looking for. Return it.
+				if (child.getNodeWithName(targetName) != null)
+					return child;
+			}
+
+			// Not found anywhere.
+			return null;
+		}
+
+		// Returns a list of ancestors of this TreeNode, starting with this
+		// node’s parent and
+		// ending with the root. Order is from recent to ancient.
+		ArrayList<TreeNode> collectAncestorsToList() {
+			ArrayList<TreeNode> ancestors = new ArrayList<>();
+
+			// ????? Collect ancestors of this TreeNode into the array list.
+			// HINT: going up
+			// the nodes of a tree is like traversing a linked list. If that
+			// isn’t clear,
+			// draw a tree, mark any leaf node, and then mark its ancestors in
+			// order from
+			// recent to ancient. Expect a question about this on the final
+			// exam.
+			TreeNode tempIterator = this;
+			// Instead of "next" we have "parent" to access the next element
+			while (tempIterator.parent != null) {
+				ancestors.add(tempIterator.parent);
+				tempIterator = tempIterator.parent;
+			}
+			
+			for (TreeNode child : children) {
+				ancestors.add(child.parent);
+			}
+			
 			return ancestors;
 		}
 
@@ -68,14 +112,10 @@ public class FamilyTree {
 
 	private TreeNode root;
 
-	//
-	// Displays a file browser so that user can select the family tree file.
-	//
-	public FamilyTree() throws IOException, TreeException
-	{
+	public FamilyTree() throws IOException, TreeException {
 		// User chooses input file. This block doesn't need any work.
-		FileNameExtensionFilter filter = 
-			new FileNameExtensionFilter("Family tree text files", "txt");
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"Family tree text files", "txt");
 		File dirf = new File("data");
 		if (!dirf.exists())
 			dirf = new File(".");
@@ -84,12 +124,14 @@ public class FamilyTree {
 		if (chooser.showOpenDialog(null) != JFileChooser.APPROVE_OPTION)
 			System.exit(1);
 		File treeFile = chooser.getSelectedFile();
-
-		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
+		// Parse the input file. Create a FileReader that reads treeFile. Create
+		// a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
-		String line;
+		FileReader fr = new FileReader(
+				"/home/surikoya/eclipse-workspace/lab12/src/BagginsFamilyTree.txt");
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine();
+
 		while ((line = br.readLine()) != null)
 			addLine(line);
 		br.close();
@@ -103,9 +145,11 @@ public class FamilyTree {
 	private void addLine(String line) throws TreeException
 	{
 		// Extract parent and array of children.
-		int colonIndex = ?? should be the index of the colon in line.
+
+		int colonIndex = line.indexOf(":");// should be the index of the colon in line.
 		if (colonIndex < 0)
-			?? throw a TreeException with a useful message
+			throw new TreeException("Bad colonIndex");
+		
 		String parent = ?? The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
 				           class java.util.String, method substring(), if you need guidance.
 		String childrenString = ?? The substring of line that starts just after colonIndex and goes through the end of
@@ -130,12 +174,21 @@ public class FamilyTree {
 		?? For each name in childrenArray, create a new node and add that node to parentNode.
 	}
 
+<<<<<<< HEAD
 	// Returns the "deepest" node that is an ancestor of the node named name1, and
 	// also is an
 	// ancestor of the node named name2.
 	//
 	// "Depth" of a node is the "distance" between that node and the root. The depth
 	// of the root is 0. The
+=======
+	// Returns the "deepest" node that is an ancestor of the node named name1,
+	// and also is an
+	// ancestor of the node named name2.
+	//
+	// "Depth" of a node is the "distance" between that node and the root. The
+	// depth of the root is 0. The
+>>>>>>> b266c1d (Saving changes)
 	// depth of the root's immediate children is 1, and so on.
 	//
 	TreeNode getMostRecentCommonAncestor(String name1, String name2) throws TreeException
@@ -170,8 +223,16 @@ public class FamilyTree {
 		try {
 			FamilyTree tree = new FamilyTree();
 			System.out.println("Tree:\n" + tree + "\n**************\n");
+<<<<<<< HEAD
 			TreeNode ancestor = tree.getMostRecentCommonAncestor("Bilbo", "Frodo");
 			System.out.println("Most recent common ancestor of Bilbo and Frodo is " + ancestor.getName());
+=======
+			TreeNode ancestor = tree.getMostRecentCommonAncestor("Bilbo",
+					"Frodo");
+			System.out.println(
+					"Most recent common ancestor of Bilbo and Frodo is "
+							+ ancestor.getName());
+>>>>>>> b266c1d (Saving changes)
 		} catch (IOException x) {
 			System.out.println("IO trouble: " + x.getMessage());
 		} catch (TreeException x) {
