5961488198326bc819ae1f2d8ead856c8912073d
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2022-04-30 07:18:48 +0000

    Update TreeException.java

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,18 +1,11 @@
 package trees;
 
-<<<<<<< HEAD
-public class TreeException extends ???
-{
-	public TreeException(String s)
-	{
-		// Pass s into a superclass ctor.
-=======
+
 public class TreeException extends Exception {
 
 	public TreeException(String s) {
 		super(s);
 		// Pass s into a superclass ctor.
 
->>>>>>> b266c1d (Saving changes)
 	}
 }
